33b5a045d3ac1496962b26f5923caaffd0bf6105
Sarah German <sgerman@smm.org>
2013-02-18 12:51:14 -0600

    Adding HTML and CSS base files from initializr

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html class="no-js">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>Maya - Beautification</title>
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="css/normalize.min.css">
+    <link rel="stylesheet" href="css/main.css">
+  </head>
+
+  <body>
+
+  </body>
+</html>
+
+
