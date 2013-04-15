323ad83fe553a8a1b7f82c770e96a861e74bf21d
Sarah German <sgerman@smm.org>
2013-04-15 17:10:05 -0500

    Markup for camera demo

--- index.php
+++ index.php
@@ -1,17 +1,27 @@
 <!DOCTYPE html>
-<html class="no-js">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title>Maya - Beautification</title>
-    <meta name="viewport" content="width=device-width">
-    <link rel="stylesheet" href="css/normalize.min.css">
-    <link rel="stylesheet" href="css/main.css">
-  </head>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="chrome=1">
+<link rel="stylesheet" href="css/maya.css" type="text/css">
+<title>Maya Beautification Demo</title>
+</head>
 
-  <body>
+<body>
+  <h1>Maya Beautification Demo</h1>
+  <section id="app" hidden>
+    <div class="container">
+      <video id="monitor" autoplay></video>
+    </div>
+  </section>
+  <p><button onclick="init(this)">Capture</button></p>
+  <div id="splash">
+    <p id="errorMessage">&uarr;<br>Click to begin</p>
+  </div>
 
-  </body>
+  <div id="gallery"></div>
+
+  <canvas id="photo" style="display:none"></canvas>
+  <script src="js/camera.js"></script>
+</body>
 </html>
-
-
